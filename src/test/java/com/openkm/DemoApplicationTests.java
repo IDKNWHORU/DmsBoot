5f1c41108087fbf3fc2d35969d741669a9c07a94
rjh <jh.ra@kisos.net>
2023-04-06 17:00:39 +0900

    move application test file

---
+++ src/test/java/com/openkm/DemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.openkm;
+
+import com.openkm.bean.NodeDocument;
+import com.openkm.repository.NodeDocumentRepository;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+    private static final Logger log = LoggerFactory.getLogger(DemoApplicationTests.class);
+
+    @Test
+    void contextLoads() {
+    }
+}
